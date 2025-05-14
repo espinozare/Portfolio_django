d2e35d89aeba5ac15fcaa90e28c1180d290f1109
espinozare <203875783+espinozare@users.noreply.github.com>
2025-05-14 18:31:49 +0100

    update urls

--- portfolio_re/urls.py
+++ portfolio_re/urls.py
@@ -25,7 +25,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("projects.urls")),
-     path("ckeditor/", include("ckeditor_uploader.urls")),
+    path("ckeditor5/", include('django_ckeditor_5.urls')),
 
     
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
